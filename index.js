3fbb2ea44abd366b7332bf63da1b6b04ab634fab
Adam Raschke <araschke@asu.edu>
2024-03-07 21:43:08 -0700

    due date

---
+++ index.js
@@ -0,0 +1,41 @@
+//unfinished
+
+//using the inquirer package
+const inquirer = require("inquirer");
+//import from images.js file
+const image = require("./image");
+//import from shapes.js file
+const { Circle, Triangle, Square } = require("./shapes");
+//import writeFile from fs/promise module
+const { writeFile } = require("fs/promises");
+
+
+// inquirer class
+return inquirer
+    //prompt input from user
+    .prompt([
+        {
+            name: "text",
+            type: "input",
+            message:"Enter text for the logo, no more than three characters.",
+            validate: function (input) {
+                return input.length <= 3;
+            }
+        },
+        {
+            name: "textColor",
+            type: "input",
+            message: "Enter a text color",
+        },
+        {
+            name: "shapeType",
+            type: "list",
+            message: "Select a shape for the logo",
+            choices: ["circle", "square", "triangle"],
+        },
+        {
+            name: "shapeColor",
+            type: "input",
+            message: "Enter a shape color",
+        },
+    ])
